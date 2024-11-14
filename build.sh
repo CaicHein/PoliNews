4bf12e11ae67f144d824fec805eca34ff518cd6b
Caic Hein Fernandes Maciel <caichein2202@gmail.com>
2024-11-14 15:41:31 -0300

    ajustes no build 4

--- build.sh
+++ build.sh
@@ -5,20 +5,16 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
+# adicione linhas abaixo
 python manage.py migrate
 
 # create superuser if missing
-python << EOF
+cat < | python manage.py shell
 import os
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-# Verifica se o superusuário já existe, caso contrário, cria um novo
-if not User.objects.filter(username=os.environ["DJANGO_SUPERUSER_USERNAME"]).exists():
-    User.objects.create_superuser(
-        os.environ["DJANGO_SUPERUSER_USERNAME"],
-        os.environ["DJANGO_SUPERUSER_EMAIL"],
-        os.environ["DJANGO_SUPERUSER_PASSWORD"]
-    )
+User.objects.filter(username=os.environ["DJANGO_SUPERUSER_USERNAME"]).exists() or \
+    User.objects.create_superuser(os.environ["DJANGO_SUPERUSER_USERNAME"], os.environ["DJANGO_SUPERUSER_EMAIL"], os.environ["DJANGO_SUPERUSER_PASSWORD"])
 EOF
