fc2d92471484de3f1fb3763d02d6fb8f5ad07aba
Caic Hein Fernandes Maciel <caichein2202@gmail.com>
2024-11-14 16:42:34 -0300

    ajustes no build 11

--- build.sh
+++ build.sh
@@ -11,22 +11,5 @@ python manage.py collectstatic --no-input
 echo "Aplicando migrações..."
 python manage.py migrate
 
-echo "Verificando superusuário..."
-python << EOF
-import os
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
-email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
-password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
-
-if username and email and password:
-    if not User.objects.filter(username=username).exists():
-        print("Criando superusuário...")
-        User.objects.create_superuser(username, email, password)
-    else:
-        print("Superusuário já existe.")
-else:
-    print("Variáveis de ambiente do superusuário não configuradas corretamente.")
-EOF
+# Importar dados do arquivo JSON
+python manage.py loaddata data.json
