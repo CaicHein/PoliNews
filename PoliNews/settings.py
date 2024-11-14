b0941ffca62b9bcf42a37f664686c0f71ad9c3b4
Caic Hein Fernandes Maciel <caichein2202@gmail.com>
2024-11-14 15:38:31 -0300

    ajustes no build 3

--- PoliNews/settings.py
+++ PoliNews/settings.py
@@ -82,11 +82,12 @@ WSGI_APPLICATION = 'PoliNews.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
 if os.environ.get("DATABASE_URL"):
     DATABASES["default"] = dj_database_url.config(
         conn_max_age=600,
