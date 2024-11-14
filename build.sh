cc1fbfb14be606d4c0517444ec24e9ded90f4d02
Caic Hein Fernandes Maciel <caichein2202@gmail.com>
2024-11-14 15:19:04 -0300

    ajustes no build.sh

--- build.sh
+++ build.sh
@@ -2,27 +2,6 @@
 # exit on error
 set -o errexit
 
-# Instala as dependências
 pip install -r requirements.txt
 
-# Coleta arquivos estáticos
 python manage.py collectstatic --no-input
-
-# Aplica migrações
-python manage.py migrate
-
-# Cria superusuário, se ainda não existir
-python << END
-import os
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-# Verifica se o superusuário já existe, caso contrário, cria um novo
-if not User.objects.filter(username=os.environ["DJANGO_SUPERUSER_USERNAME"]).exists():
-    User.objects.create_superuser(
-        os.environ["DJANGO_SUPERUSER_USERNAME"],
-        os.environ["DJANGO_SUPERUSER_EMAIL"],
-        os.environ["DJANGO_SUPERUSER_PASSWORD"]
-    )
-END
