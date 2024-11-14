cf11f8ea4e6ca8c55701f279f9ce0358b5c8c467
Caic Hein Fernandes Maciel <caichein2202@gmail.com>
2024-11-14 16:32:23 -0300

    ajustes no build 9

--- build.sh
+++ build.sh
@@ -2,10 +2,31 @@
 # exit on error
 set -o errexit
 
-# Instala as dependências
+echo "Instalando dependências..."
 pip install -r requirements.txt
 
-# Coleta arquivos estáticos
+echo "Coletando arquivos estáticos..."
 python manage.py collectstatic --no-input
 
+echo "Aplicando migrações..."
+python manage.py migrate
 
+echo "Verificando superusuário..."
+python << EOF
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+if username and email and password:
+    if not User.objects.filter(username=username).exists():
+        print("Criando superusuário...")
+        User.objects.create_superuser(username, email, password)
+    else:
+        print("Superusuário já existe.")
+else:
+    print("Variáveis de ambiente do superusuário não configuradas corretamente.")
+EOF
